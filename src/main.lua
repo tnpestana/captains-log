cfde9354022d6724f9fc7946fda11c14fc45e03f
Tiago Pestana <tnevespestana@gmail.com>
2025-09-14 22:09:55 +0100

    refactoring

--- src/main.lua
+++ src/main.lua
@@ -5,28 +5,34 @@ if script_dir then
 end
 
 local libexec = require("utils/libexec")
-local file_utils = require("utils/file")
-local str_utils = require("utils/string")
-local journal_utils = require("utils/journal")
-local config_utils = require("utils/config")
+local file = require("utils/file")
+local str = require("utils/string")
+local journal = require("utils/journal")
+local config = require("utils/config")
+local cli = require("utils/cli")
 
-local function parse_args(args)
-  local options = {
-    write_mode = false,
-    entry_text = ""
-  }
+local function handle_write_mode(entry_path, options)
+    file.create_dir(entry_path)
 
-  for i, arg in ipairs(args) do
-    if arg == "-w" then
-      options.write_mode = true
-      local entry_text = args[i + 1]
-      if entry_text and str_utils.is_nonempty_string(entry_text) then
-        options.entry_text = entry_text
-      end
+    local header = journal.format_date_header()
+    file.create_file(entry_path, header)
+
+    local time_str = os.date("%H:%M")
+    local entry_timestamp = "\n## " .. time_str
+    file.append_to_file(entry_path, entry_timestamp)
+
+    if str.is_nonempty_string(options.entry_text) then
+      local entry_text = "\n" .. options.entry_text
+      file.append_to_file(entry_path, entry_text)
+      os.exit(0)
     end
-  end
+end
 
-  return options
+local function handle_normal_mode(entry_path)
+  local date_str = os.date("%A, %B %d, %Y")
+  local entry_header = "# Captain's Log - " .. date_str .. "\n\n"
+  file.create_dir(entry_path)
+  file.create_file(entry_path, entry_header)
 end
 
 local function launch_editor(path)
@@ -37,33 +43,21 @@ end
 local function main()
   libexec.setup()
 
-  local options = parse_args(arg)
+  local options = cli.parse_args(arg)
+  if options.help then
+    cli.print_help()
+    return
+  end
 
-  local output_dir = config_utils.BASE_DIR
-  file_utils.create_dir(output_dir)
+  local output_dir = config.BASE_DIR
+  file.create_dir(output_dir)
 
-  local entry_path = journal_utils.get_todays_entry_path(output_dir)
+  local entry_path = journal.get_todays_entry_path(output_dir)
 
   if options.write_mode then
-    file_utils.create_dir(entry_path)
-
-    local header = journal_utils.format_date_header()
-    file_utils.create_file(entry_path, header)
-
-    local time_str = os.date("%H:%M")
-    local entry_timestamp = "\n\n## " .. time_str
-    file_utils.append_to_file(entry_path, entry_timestamp)
-
-    if str_utils.is_nonempty_string(options.entry_text) then
-      local entry_text = "\n" .. options.entry_text
-      file_utils.append_to_file(entry_path, entry_text)
-      return -- early return since the entry is complete
-    end
+    handle_write_mode(entry_path, options)
   else
-    local date_str = os.date("%A, %B %d, %Y")
-    local entry_header = "# Captain's Log - " .. date_str .. "\n\n"
-    file_utils.create_dir(entry_path)
-    file_utils.create_file(entry_path, entry_header)
+    handle_normal_mode(entry_path)
   end
 
   launch_editor(entry_path)
